5900e40ea4388b661b6919e4dfc343786b17bc4c
lucas.anacleto <lucas.anacleto@outlook.com>
2023-07-22 00:34:56 -0300

    Dashboard setup

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -35,7 +35,7 @@ export const StoreModal = () => {
       setLoading(true);
       const response = await axios.post('/api/stores', values);
 
-      toast.success("Store criado.")
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("algo deu errado.");
     } finally {
