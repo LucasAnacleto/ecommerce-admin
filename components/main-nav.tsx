70dae742856dce9a50efb09d734fb2f16bfc7d11
lucas.anacleto <lucas.anacleto@outlook.com>
2023-07-23 01:42:24 -0300

    Settings page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,6 +13,11 @@ export function MainNav({
   
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: 'Geral',
+      active: pathname === `/${params.storeId}`, 
+    },    
+    {
       href: `/${params.storeId}/settings`,
       label: 'Ajustes',
       active: pathname === `/${params.storeId}/settings`, 
