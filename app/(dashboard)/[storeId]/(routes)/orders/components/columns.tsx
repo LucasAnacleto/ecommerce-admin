0af2394a9db01e94242d993cc6a1bee445e61d3d
lucas.anacleto <lucas.anacleto@outlook.com>
2023-07-30 00:27:23 -0300

    Orders Entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type OrderColumn = {
+  id: string
+  phone: string
+  address: string
+  isPaid: boolean
+  totalPrice: string
+  products: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Produtos",
+  },
+  {
+    accessorKey: "phone",
+    header: "Telefone",
+  },
+  {
+    accessorKey: "address",
+    header: "Endereco",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Preco total",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Pago",
+  },
+]
